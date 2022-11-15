ab86a77f8fda19a81bc6e22e7b9a600f37949719
saptarinoIT <saptarino.it@gmail.com>
2022-11-15 10:18:22 +0800

    update scan validation

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -16,13 +16,26 @@ class ScanController extends Controller
     public function update($id)
     {
         $wisuda = Wisuda::find($id);
+        $nim = substr($id, 9, null);
         if ($wisuda) {
             $wisuda->kehadiran = 'ya';
             $wisuda->save();
             Alert::success('Mahasiswa Hadir', $wisuda->nim . ' - ' . ucwords($wisuda->nama_lengkap));
             return redirect()->route('scan.index')->with('status', 'Mahasiswa Hadir.');
+        } elseif ('orangtua_' . $nim) {
+            $wisuda = Wisuda::find($nim);
+            $wisuda->orangtua = 'ya';
+            $wisuda->save();
+            Alert::success('Orang Tua Mahasiswa Hadir', $wisuda->nim . ' - ' . ucwords($wisuda->nama_lengkap));
+            return redirect()->route('scan.index')->with('status', 'Mahasiswa Hadir.');
+        } elseif ('pendamping_' . $nim) {
+            $wisuda = Wisuda::find($nim);
+            $wisuda->pendamping = 'ya';
+            $wisuda->save();
+            Alert::success('Pendamping Mahasiswa Hadir', $wisuda->nim . ' - ' . ucwords($wisuda->nama_lengkap));
+            return redirect()->route('scan.index')->with('status', 'Mahasiswa Hadir.');
         } else {
-            Alert::warning('Maaf Mahasiswa Tidak Terdaftar!!!');
+            Alert::warning('Maaf Data Tidak Terdaftar!!!');
             return redirect()->route('scan.index');
         }
     }
